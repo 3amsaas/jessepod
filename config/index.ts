50eed09fa5848cdce3d39f76b905ee2eadd5c552
3amsaas <152881079+3amsaas@users.noreply.github.com>
2024-07-06 14:19:47 +0700

    Update index.ts

--- config/index.ts
+++ config/index.ts
@@ -4,9 +4,9 @@ export const API_KEY = `${process.env.NEXT_PUBLIC_APP_KEY}`
 export const API_URL = `${process.env.NEXT_PUBLIC_API_URL}`
 export const IS_WORKFLOW = `${process.env.NEXT_PUBLIC_APP_TYPE_WORKFLOW}` === 'true'
 export const APP_INFO: AppInfo = {
-  title: 'Text Generator APP',
-  description: 'App description',
-  copyright: '',
+  title: 'Jesse PODMan 2.0',
+  description: 'Meet Jesse PODMan: Effortlessly optimize your Amazon listings with AI. Copy-Paste your details and get polished listings instantly.',
+  copyright: 'Created By Bank K. | TinyMarketingLab.com 2024',
   privacy_policy: '',
   default_language: 'en-US',
 }
